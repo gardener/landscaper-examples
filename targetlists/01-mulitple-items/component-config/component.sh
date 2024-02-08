ee1705db34cfe08be27849db79a56750a6b34f51
Robert Graeff <robert.graeff@sap.com>
2024-02-08 10:45:33 +0100

    Targetlists: multiple subinstallations

--- targetlists/01-mulitple-items/component-config/component.sh
+++ targetlists/01-mulitple-items/component-config/component.sh
@@ -19,17 +19,19 @@ if [[ -z "${repository_base_url}" ]]; then
   exit 1
 fi
 
+ctf_dir=`mktemp -d`
+
 # This commands adds the components to a ctf (common transport archive), which is a file system representation of a
 # oci registry
 # --create specifies that the ctf file/directory should be created if it does not exist yet
 # --file specifies the target ctf file/directory where the components should be added
-ocm add components --create --file ../component-ctf ./components.yaml
+ocm add components --create --file $ctf_dir ./components.yaml
 
 # This command transfers the components contained in the specified ctf (here tour-ctf) to another component repository
 # (here, an oci registry)
 # --enforce specifies that already existing components in the target should always be overwritten with the ones
 # from your source
-ocm transfer ctf --enforce ../component-ctf ${repository_base_url}
+ocm transfer ctf --enforce $ctf_dir ${repository_base_url}
 
 ## to inspect a specific component, you can use the following command to download into a component archive (a simple file
 ## system representation of a single component)
